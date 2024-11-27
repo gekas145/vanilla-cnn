f33527dc5c3c9e14013f03c068c9dcb58545a357
gekas145 <56126336+gekas145@users.noreply.github.com>
2024-11-27 20:21:52 +0100

    refactor code + add maxpool backward

--- ops.py
+++ ops.py
@@ -25,25 +25,26 @@ dense_weights = np.random.normal(loc=0.0, scale=1.0, size=(H1, H))
 dense_biases = np.random.normal(loc=0.0, scale=1.0, size=(H1))
 dense_output_der = np.random.normal(loc=0.0, scale=1.0, size=(B, H1))
 
+maxpool_N = 2
+maxpool_input = np.random.normal(loc=0.0, scale=1.0, size=(B, I, I, C2))
+
+
+
 def activation(x):
     return 1/(1 + np.exp(-x))
 
 def activation_der(x):
     return 1/(1 + np.exp(-x))
 
-@jit(nopython=True)
 def flatten_forward(input):
     return input.reshape(input.shape[0], -1)
 
-@jit(nopython=True)
 def flatten_backward(output, input_shape):
     return output.reshape(input_shape)
 
-@jit(nopython=True)
 def dense_forward(input, weights, biases):
     return input @ weights.T + biases
 
-@jit(nopython=True)
 def dense_backward(output_der, input, weights):
     B = input.shape[0]
 
@@ -63,21 +64,35 @@ def maxpool_forward(input, N):
     B = input.shape[0]
     I = input.shape[1] // N
     C = input.shape[3]
-    indexes = np.zeros((B, I**2, 2, C), dtype=int)
-    output = np.zeros((B, I, I, C), dtype=float)
+    indexes = np.zeros((B, I**2, C, 2), dtype=np.int32)
+    output = np.zeros((B, I, I, C), dtype=np.float32)
 
     for s1 in range(I):
         for s2 in range(I):
-            maxpool_window = input[:, N*s1:N*(s1+1), N*s2:N*(s2+1), :].reshape(B, -1, C)
+            maxpool_window = input[:, N*s1:N*(s1+1), N*s2:N*(s2+1), :]
+            maxpool_window = maxpool_window.reshape(B, N**2, C)
             indexes_window = np.argmax(maxpool_window, axis=1)
             i = indexes_window // N + N*s1
             j = indexes_window % N + N*s2
             indexes[:, I*s1 + s2, ...] = np.stack((i, j), axis=2)
 
-            values_window = np.max(maxpool_window, axis=1)
+            values_window = np.amax(maxpool_window, axis=1)
             output[:, s1, s2, :] = values_window
 
     return output, indexes
+
+def maxpool_backward(output_der, input_shape, indexes):
+    B = input_shape[0]
+    C = input_shape[3]
+
+    input_der = np.zeros(input_shape, dtype=np.float32)
+    output_der_reshaped = output_der.reshape(B, -1, C)
+
+    for b in range(B):
+        for c in range(C):
+            input_der[b, indexes[b, :, c, 0], indexes[b, :, c, 1], c] = output_der_reshaped[b, :, c]
+
+    return input_der
 
 @jit(nopython=True)
 def cnn_forward(input, kernels, biases):
@@ -120,6 +135,7 @@ def cnn_backward(output_der, input, kernels):
 
 
 if __name__ == "__main__":
+    # CNN
     for i in range(2):
         start = time.time()
         cnn_output = cnn_forward(cnn_input, cnn_kernels, cnn_biases)
@@ -133,26 +149,33 @@ if __name__ == "__main__":
         assert cnn_input.shape == cnn_input_der.shape
         assert cnn_kernels.shape == cnn_kernels_der.shape
         assert cnn_biases.shape == cnn_biases_der.shape
-        
+    
+    # Dense     
+    start = time.time()
+    dense_output = dense_forward(dense_input, dense_weights, dense_biases)
+    dense_input_der, dense_weights_der, dense_biases_der = dense_backward(dense_output_der, dense_input, dense_weights)
 
-    for i in range(2):
-        start = time.time()
-        dense_output = dense_forward(dense_input, dense_weights, dense_biases)
-        dense_input_der, dense_weights_der, dense_biases_der = dense_backward(dense_output_der, dense_input, dense_weights)
+    print(f"Dense Elapsed time: {time.time() - start} seconds")
+    assert dense_output.shape == dense_output_der.shape
+    assert dense_input.shape == dense_input_der.shape
+    assert dense_weights.shape == dense_weights_der.shape
+    assert dense_biases.shape == dense_biases_der.shape
 
-        if i == 0:
-            continue
-
-        print(f"Dense Elapsed time: {time.time() - start} seconds")
-        assert dense_output.shape == dense_output_der.shape
-        assert dense_input.shape == dense_input_der.shape
-        assert dense_weights.shape == dense_weights_der.shape
-        assert dense_biases.shape == dense_biases_der.shape
-
+    # Flatten
     flatten_output = flatten_forward(flatten_input)
     flatten_input2 = flatten_backward(flatten_output, flatten_input.shape)
     assert flatten_output.shape == (flatten_input.shape[0], np.prod(flatten_input.shape[1:]))
     assert np.allclose(flatten_input, flatten_input2)
+
+    # Maxpool
+    start = time.time()
+    maxpool_output, maxpool_indexes = maxpool_forward(maxpool_input, maxpool_N)
+    maxpool_input_der = maxpool_backward(maxpool_output, maxpool_input.shape, maxpool_indexes)
+
+    print(f"Maxpool Elapsed time: {time.time() - start} seconds")
+    assert maxpool_output.shape == (B, I//maxpool_N, I//maxpool_N, C2)
+    assert maxpool_indexes.shape == (B, (I//maxpool_N)**2, C2, 2)
+    assert maxpool_input.shape == maxpool_input_der.shape
 
 
 
